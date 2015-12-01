bf15593ba3efc581d82b9decb316c3a44f2d2887
kensuketamura <kensuke.tamura.183@gmail.com>
2015-12-01 17:56:04 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+import nez.Parser;
+import nez.ast.Tree;
+import nez.main.CommandContext;
+
+
+public class Main {
+
+	public final static void main(String[] args) {
+		try {
+			CommandContext c = new CommandContext();
+			c.parseCommandOption(args, false/* nezCommand */);
+			Parser p = c.newParser();
+			Tree<?> node = p.parseCommonTree(c.nextInput());
+			if(node != null){
+				eval(node);
+			}
+		} catch (IOException e) {
+			System.exit(1);
+		}
+	}
+
+}
