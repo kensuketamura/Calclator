73048b7037814ee821ab24004945f3ecbb60a63b
kensuketamura <kensuke.tamura.183@gmail.com>
2015-12-07 15:33:45 +0900

    modified translator

--- src/Translator.java
+++ src/Translator.java
@@ -14,6 +14,18 @@ public class Translator {
 			return new Div(translate(node.get(0)), translate(node.get(1)));
 		case "Int":
 			return new Int(Integer.parseInt(node.toText()));
+		case "LessThan":
+			return new LessThan(translate(node.get(0)), translate(node.get(1)));
+		case "GreaterThan":
+			return new GreaterThan(translate(node.get(0)), translate(node.get(1)));
+		case "LessThanEquals":
+			return new LessThanEquals(translate(node.get(0)), translate(node.get(1)));
+		case "GreaterThanEquals":
+			return new GreaterThanEquals(translate(node.get(0)), translate(node.get(1)));
+		case "Equals":
+			return new Equals(translate(node.get(0)), translate(node.get(1)));
+		case "NotEquals":
+			return new NotEquals(translate(node.get(0)), translate(node.get(1)));
 		default:
 			break;
 		}
